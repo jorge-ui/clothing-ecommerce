28f764e75e82d8a942e0d01c710748ee5deda3a8
Jorge Rivera <jrdeveloper71@gmail.com>
2019-08-05 16:09:18 -0400

    add onClick navigation to menu-item withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.styles.css";
-import HomePage from "./pages/home-page/home-page.component";
+import React from 'react';
+import './App.styles.css';
+import HomePage from './pages/home-page/home-page.component';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div className="App">
-    <HomePage />
+    <Route exact path="/" component={HomePage} />
   </div>
 );
 
