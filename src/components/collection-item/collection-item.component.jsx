6a6841a082b94cb1f884964de5fac9f500b3d5ed
Jorge Rivera <jrdeveloper71@gmail.com>
2019-08-16 03:12:35 -0400

    collection-item animation pending

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './collection-item.styles.scss';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
 
 const CollectionItem = ({ item, addItem }) => {
+  const [willAdd, setWillAdd] = useState(false);
+  const [didAdd, setDidAdd] = useState(false);
   const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
@@ -14,10 +16,20 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </footer>
-      <div className="add-to-cart-button" onClick={() => addItem(item)}>
+      <div
+        className="add-to-cart-button"
+        onClick={() => addItem(item)}
+        onMouseDown={() => setWillAdd(true)}
+        onMouseUp={() => setDidAdd(true)}
+      >
         <Icon icon="cart-plus" className="cart-plus" />
         <Icon icon="plus" className="plus" />
       </div>
+      <div
+        className="overlay"
+        willadd={String(willAdd)}
+        didadd={String(didAdd)}
+      />
     </div>
   );
 };
