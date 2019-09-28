439a469f777aa832cb180f7160646dbcb5d2a412
Jorge Rivera <jrdeveloper71@gmail.com>
2019-09-27 20:30:48 -0400

    Updates

---
+++ precache-manifest.35425388a7de884bfc33f4d8d8f60ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b3c63a7443c5ee4351940906f4c367",
+    "url": "/clothing-ecommerce/index.html"
+  },
+  {
+    "revision": "09ace9696698e2c98d62",
+    "url": "/clothing-ecommerce/static/css/main.21c8036a.chunk.css"
+  },
+  {
+    "revision": "289aca932c67dd142040",
+    "url": "/clothing-ecommerce/static/js/2.8ffce347.chunk.js"
+  },
+  {
+    "revision": "09ace9696698e2c98d62",
+    "url": "/clothing-ecommerce/static/js/main.003aed08.chunk.js"
+  },
+  {
+    "revision": "b2cf7ee8c47511b268c9",
+    "url": "/clothing-ecommerce/static/js/runtime~main.394ad9a3.js"
+  },
+  {
+    "revision": "7165883ac6c726b8a118f62a2d065361",
+    "url": "/clothing-ecommerce/static/media/black-spinner.7165883a.png"
+  },
+  {
+    "revision": "340195f789f029d55370e9dcdd9e2862",
+    "url": "/clothing-ecommerce/static/media/crown.340195f7.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
